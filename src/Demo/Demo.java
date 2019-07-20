5222002fd8a43143928f190e4fd987bed1c1ba4c
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-20 23:19:06 +0800

    Update Demo.java

--- src/Demo/Demo.java
+++ src/Demo/Demo.java
@@ -1,5 +1,11 @@
-pakcage Demo;
+package Demo;
 
-I like Florence and I will be at University of Toronto with her next year wait for me!
-  
-  It is funny that I don't what is going to happen but I will wait for it!
+public class Demo {
+	public static void main(String[]args) {
+		new Demo();
+	}
+	
+	private Demo() {
+		
+	}
+}
